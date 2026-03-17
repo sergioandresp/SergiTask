39606e309c3cc5035958a27a5728027f6ca669d1
sergioandresp <sergioespinoza2001@gmail.com>
2026-03-17 09:50:04 -0300

    Callback corregido sin errores rojos

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,16 +1,17 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-    const requestUrl = new URL(request.url)
-    const code = requestUrl.searchParams.get('code')
+    const { searchParams, origin } = new URL(request.url)
+    const code = searchParams.get('code')
 
     if (code) {
-        const supabase = createRouteHandlerClient({ cookies })
+        const supabase = createClient(
+            process.env.NEXT_PUBLIC_SUPABASE_URL!,
+            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+        )
         await supabase.auth.exchangeCodeForSession(code)
     }
 
-    // Esto te devuelve a la página principal una vez logueado
-    return NextResponse.redirect(requestUrl.origin)
+    return NextResponse.redirect(origin)
 }
